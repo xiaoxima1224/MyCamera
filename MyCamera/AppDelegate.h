d7fde258e88b0f911e23e3eef7e634e02bc54547
Ma, Xiaoxi <xma@quidsi.com>
2015-01-24 14:03:54 -0500

    Initial Commit

---
+++ MyCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCamera
+//
+//  Created by Ma, Xiaoxi on 1/24/15.
+//  Copyright (c) 2015 Ma, Xiaoxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyCamera
+//
+//  Created by Ma, Xiaoxi on 1/24/15.
+//  Copyright (c) 2015 Ma, Xiaoxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
